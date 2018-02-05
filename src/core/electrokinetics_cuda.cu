9d60451559e70ad21e37c5895b8229d83b33d386
Lukas Eisert <lukas.eisert@live.de>
2018-02-05 14:49:13 +0100

    The template for the first of eight nodes is finished. Prefactor unclear.

--- src/core/electrokinetics_cuda.cu
+++ src/core/electrokinetics_cuda.cu
@@ -2447,7 +2447,10 @@ __global__ void ek_apply_ev( CUDA_particle_data * particle_data,
   float cellpos[3];
   float gridpos;
   
-  float ev_force = 1.0; 
+  float invnorm
+  float ev_force_norm = 1.0;
+  float ev_force[3];
+  float prefactor;
 
   if( index < ek_lbparameters_gpu->number_of_particles ) 
   {  
@@ -2469,11 +2472,27 @@ __global__ void ek_apply_ev( CUDA_particle_data * particle_data,
 
     index = rhoindex_cartesian2linear( lowernode[0], lowernode[1], lowernode[2] )    
     
-    atomicadd( &ek_parameters_gpu.j[jindex_getByRhoLinear( index, EK_LINK_U00 )],
-               ek_parameters_gpu.rho[species_index][index] *
-               ( 1 - cellpos[0] ) * ( 1 - cellpos[1] ) * ( 1 - cellpos[2] )
-               * ek_parameters_gpu.time_step
-    );
+    ev_force[0] = -cellpos[0];
+    ev_force[1] = -cellpos[1];
+    ev_force[2] = -cellpos[2];
+    
+    invnorm = 1.0f / fsqrt( ev_force[0] * ev_force[0] + ev_force[1] * ev_force[1] + ev_force[2] * ev_force[2] )
+    
+    ev_force[0] *= invnorm * ev_force_norm;
+    ev_force[1] *= invnorm * ev_force_norm;
+    ev_force[2] *= invnorm * ev_force_norm;
+    
+    prefactor = ek_parameters_gpu.rho[species_index][index] * ek_parameters_gpu.time_step
+    
+    atomicadd( &ek_parameters_gpu.j[jindex_getByRhoLinear( index, EK_LINK_U00 )], prefactor * ev_force[0] );
+    atomicadd( &ek_parameters_gpu.j[jindex_getByRhoLinear( index, EK_LINK_0U0 )], prefactor * ev_force[1] );
+    atomicadd( &ek_parameters_gpu.j[jindex_getByRhoLinear( index, EK_LINK_00U )], prefactor * ev_force[2] );
+    atomicadd( &ek_parameters_gpu.j[jindex_getByRhoLinear( index, EK_LINK_UU0 )], prefactor * (ev_force[0] + ev_force[1])/sqrtf(2.0f) );
+    atomicadd( &ek_parameters_gpu.j[jindex_getByRhoLinear( index, EK_LINK_U0U )], prefactor * (ev_force[0] + ev_force[2])/sqrtf(2.0f) );
+    atomicadd( &ek_parameters_gpu.j[jindex_getByRhoLinear( index, EK_LINK_0UU )], prefactor * (ev_force[1] + ev_force[2])/sqrtf(2.0f) );
+    atomicadd( &ek_parameters_gpu.j[jindex_getByRhoLinear( index, EK_LINK_UD0 )], prefactor * (ev_force[0] - ev_force[1])/sqrtf(2.0f) );
+    atomicadd( &ek_parameters_gpu.j[jindex_getByRhoLinear( index, EK_LINK_U0D )], prefactor * (ev_force[0] - ev_force[2])/sqrtf(2.0f) );
+    atomicadd( &ek_parameters_gpu.j[jindex_getByRhoLinear( index, EK_LINK_0UD )], prefactor * (ev_force[1] - ev_force[2])/sqrtf(2.0f) );
              
 
 
